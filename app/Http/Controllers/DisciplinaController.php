300d5bbf79d635728341c7e8ed555ce228d653e7
Leandro Ramos <leandroramos@usp.br>
2018-08-05 09:31:41 -0300

    issue5 - edição de disciplina

--- app/Http/Controllers/DisciplinaController.php
+++ app/Http/Controllers/DisciplinaController.php
@@ -26,7 +26,7 @@ class DisciplinaController extends Controller
      */
     public function create()
     {
-        //
+        return view('disciplinas.create');
     }
 
     /**
@@ -37,7 +37,11 @@ class DisciplinaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         $disciplina = new Disciplina;
+         $disciplina->titulo = $request->titulo;
+         $disciplina->ementa = $request->ementa;
+         $disciplina->save();
+         return redirect('/');
     }
 
     /**
@@ -59,7 +63,7 @@ class DisciplinaController extends Controller
      */
     public function edit(Disciplina $disciplina)
     {
-        //
+        return view('disciplinas.edit',compact('disciplina'));
     }
 
     /**
@@ -71,7 +75,10 @@ class DisciplinaController extends Controller
      */
     public function update(Request $request, Disciplina $disciplina)
     {
-        //
+        $disciplina->titulo = $request->titulo;
+        $disciplina->ementa = $request->ementa;
+        $disciplina->save();
+        return redirect("/disciplinas/$disciplina->id");
     }
 
     /**
--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -1,9 +1,14 @@
+<p>
+	<a href="disciplinas/create">Cadastrar nova disciplina</a>
+</p>
 <ul>
 @foreach ($disciplinas as $disciplina)
     <li>
     	<a href="/disciplinas/{{ $disciplina->id }}">
     		{{ $disciplina->titulo }}
     	</a>
+    	<br>
+    	<a href="/disciplinas/{{ $disciplina->id }}/edit"> Editar </a>
     </li>
 @endforeach
 </ul>
