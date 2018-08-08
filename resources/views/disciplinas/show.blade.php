1e22a43309724c0cb9b4d3a36865ab166a51f69c
Leandro Ramos <leandroramos@usp.br>
2018-08-08 17:19:22 -0300

    issue10 - turmas listadas

--- resources/views/disciplinas/show.blade.php
+++ resources/views/disciplinas/show.blade.php
@@ -1,3 +1,11 @@
 <h1>{{ $disciplina->titulo }}</h1>
 <p>{{ $disciplina->ementa }}</p>
 <a href="/disciplinas/{{ $disciplina->id }}/turmas/create">Inserir Turma</a>
+
+<h2>Turmas:</h2>
+@foreach ($disciplina->turmas as $turma)
+    <div>
+    {{ $turma->ministrante }}
+    {{ $turma->inicio }}
+    </div>
+@endforeach
